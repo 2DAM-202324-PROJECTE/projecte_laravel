d5ff40607b46795b3db50211dd521fc24fe2c694
«sara21h» <sbogdan@iesebre.com>
2024-02-20 20:44:13 +0100

    FIX #8

--- tests/Feature/Livewire/Categories/IndexTest.php
+++ tests/Feature/Livewire/Categories/IndexTest.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Tests\Feature\Livewire\Categories;
-
 use App\Livewire\Categories\Index;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -15,5 +15,21 @@ class IndexTest extends TestCase
     {
         Livewire::test(Index::class)
             ->assertStatus(200);
+    }
+    /** @test */
+    public function component_exists_on_the_page()
+    {
+        $this->get('/categories')
+            ->assertSeeLivewire('Index:class');
+    }
+    /** @test */
+    public function displays_2_category_names()
+    {
+        $categories = Category::factory()->count(2)->create();
+        $names = $categories->pluck('name');
+        $test = Livewire::test(Index::class);
+        foreach ($names as $name) {
+            $test->assertSee($name);
+        }
     }
 }
