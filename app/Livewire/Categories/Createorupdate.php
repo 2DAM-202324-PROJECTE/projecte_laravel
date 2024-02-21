5f323e80af99a728fc9b1f793c0a1d1eb1629e35
«sara21h» <sbogdan@iesebre.com>
2024-02-21 19:50:07 +0100

    FIX #9

--- app/Livewire/Categories/Createorupdate.php
+++ app/Livewire/Categories/Createorupdate.php
@@ -3,16 +3,47 @@
 namespace App\Livewire\Categories;
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Livewire\Component;
 
 class Createorupdate extends Component
 {
     public $nom = '';
+    public ?Category $category;
+    public bool $isCreation;
 
     public function save(){
         Category::create($this->only(['nom']));
         return $this->redirectRoute('categories');
     }
+    public function update()
+    {
+        $this->category->update(
+            $this->only(['nom'])
+        );
+        return $this->redirectRoute('categories');
+    }
+    public function setCategory($id)
+    {
+        $category = Category::findOrFail($id);
+        $this->category = $category;
+        $this->nom = $category->nom;
+    }
+
+    public function mount($id = null)
+    {
+        if($id != null){
+            $this->isCreation = false;
+            try{
+                $this->setCategory($id);
+            }catch (ModelNotFoundException $e){
+                session()->flash('message-danger', 'Category not found');
+            }
+        }else{
+            $this->isCreation = true;
+        }
+    }
+
     public function render()
     {
         return view('livewire.categories.createorupdate');
--- resources/views/livewire/categories/createorupdate.blade.php
+++ resources/views/livewire/categories/createorupdate.blade.php
@@ -3,5 +3,17 @@
         <label for="nom">Nom:</label>
         <input type="text" id="title" wire:model="nom">
         <button type="submit">Save</button>
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @elseif (session()->has('message-danger'))
+            <div class="alert alert-danger">
+                {{ session('message-danger') }}
+            </div>
+        @endif
+        <form wire:submit="{{ $isCreation ? 'save' : 'update' }}">
+        </form>
     </form>
 </div>
+
