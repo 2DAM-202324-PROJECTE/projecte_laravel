c0a4c3a993c75dd53a6e7a88d8562218ec8d43dd
«sara21h» <sbogdan@iesebre.com>
2024-02-26 19:49:43 +0100

    FIX #13

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,8 @@ class Category extends Model
         'name', 'description',
     ];
     use HasFactory;
+    public function media()
+    {
+        return $this->hasMany(Media::class);
+    }
 }
