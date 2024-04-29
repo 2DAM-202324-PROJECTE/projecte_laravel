1866b49cd85f1b3333f78e11631f7a57e2a6ae1a
SergioBertomeu <sbertomeu1@iesebre.com>
2024-04-29 20:45:33 +0200

    arreglar error migracions

--- database/migrations/2024_03_15_152516_create_messages_table.php
+++ database/migrations/2024_03_15_152516_create_messages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('missatges', function (Blueprint $table) {
             $table->id();
 
             $table->unsignedBigInteger('xatinteractiu_id');
