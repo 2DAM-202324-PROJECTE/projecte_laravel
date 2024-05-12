1086c99b1b70225061427d111bcf81878b787233
SergioBertomeu <sbertomeu1@iesebre.com>
2024-05-12 09:12:09 +0200

    taula per als rols Creada

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_role', 'role_id', 'user_id');
+    }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,15 +16,18 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'admin',
+            'role_id' => 1, // 'admin' role
             'email' => 'admin@test.net',
             'password' => Hash::make('admin')]);
         User::create([
             'name' => 'user',
+            'role_id' => 2, // 'user' role
             'email' => 'user@admin.test',
             'password' => Hash::make('user')]);
 
         User::create([
             'name' => 'admin',
+            'role_id' => 3, // 'suscriptor' role
             'email' => 'admin@teamflix.com',
             'password' => Hash::make('admin')]);
 
