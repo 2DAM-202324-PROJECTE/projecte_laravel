6f8adef68405e84385d6c4f052ea7cf76c48226d
«sara21h» <sbogdan@iesebre.com>
2024-05-29 17:22:54 +0200

    missatge permis

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -11,7 +11,7 @@ class CheckRole
     public function handle(Request $request, Closure $next, $role)
     {
         if (!Auth::check() || Auth::user()->role->nom != $role) {
-            abort(403, 'No tienes permiso para acceder a esta página.');
+            abort(403, 'No tens permisos per accedir a aquesta pàgina!');
         }
 
         return $next($request);
